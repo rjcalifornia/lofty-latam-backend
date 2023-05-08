c2b241c625f4341c63b2d5e701e2b4c3bc6a3b44
rjcalifornia <rodolfohernan20@gmail.com>
2023-05-08 12:55:38 -0600

    Changes to be committed: 	modified:   app/Http/Controllers/api/UsersController.php

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Facades\DB;
+
 use App\Models\User;
 
 class UsersController extends Controller{
@@ -16,4 +18,28 @@ class UsersController extends Controller{
         $userProfile = User::with(['rol'])->where('id', $user->id)->first();
         return response()->json($userProfile, 200);
     }
+
+    public function changePassword(Request $request){
+
+        $validator = Validator::make($request->json()->all(),[
+            'password' => 'required|string|same:repeat_password|min:6',
+            'repeat_password' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 422);
+        }
+        $userId = Auth::user();
+        $user = User::where('id', $userId->id)->first();
+        if (!$user or !$user->active) {
+            return response()->json(['message' => 'Verifique los datos ingresados e intente nuevamente', 404]);
+        }
+        DB::transaction(function () use ($request, $user) {
+            $user->password = bcrypt($request->get('password'));
+            $user->save();
+        });
+        
+        return response()->json([],204);
+
+    }
+
 }
