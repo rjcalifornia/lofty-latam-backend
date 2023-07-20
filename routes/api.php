e7f17cc3cdd02fa84d5f57ec6acbd3964087f632
rjcalifornia <rodolfohernan20@gmail.com>
2023-07-20 15:23:23 -0600

    Changes to be committed: 	modified:   app/Http/Controllers/api/UsersController.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,9 @@ Route::prefix('/v1/administration')->group(function () {
 });
 
 Route::prefix('/v1/user')->group(function () {
-    Route::get('/', [AuthController::class, 'getUserDetails'])->middleware('auth:sanctum');
+    Route::get('/', [UsersController::class, 'getUserDetails'])->middleware('auth:sanctum');
     Route::post('/update', [UsersController::class, 'updateUserDetails'])->middleware('auth:sanctum');
+    Route::post('/change-password', [UsersController::class, 'changePassword'])->middleware('auth:sanctum');
     Route::get('/profile', [UsersController::class, 'userProfile'])->middleware('auth:sanctum');
 });
 
