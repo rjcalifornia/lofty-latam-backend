bc9533edf69ca8989d5d4ff3c872eda1bb76868e
rjcalifornia <rodolfohernan20@gmail.com>
2023-08-04 10:58:38 -0600

    Changes to be committed: 	modified:   app/Models/LeaseAgreements.php 	modified:   app/Models/Property.php

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -70,7 +70,7 @@ class Property extends Model
     }
 
     public function leases(){
-        return $this->hasMany(LeaseAgreements::class, 'property_id');
+        return $this->hasMany(LeaseAgreements::class, 'property_id')->orderBy('active', 'DESC');
     }
 
     public function propertyPictures()
